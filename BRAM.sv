a031e2b94fe917c3d39f9cec35b5067c710a49f6
berkarslanpencesi <berk.arslanpencesi41@gmail.com>
2025-12-08 12:23:41 +0300

    Update BRAM.sv

--- BRAM.sv
+++ BRAM.sv
@@ -1,14 +1,13 @@
 `timescale 1ns / 1ps
-
+    //Read first BRAM
+    //WRITE_FIRST Yapmak için Bypass konulmali
 module BRAM#(
     parameter DATA_WIDTH=16,
     parameter ADDR_WIDTH=9
     )
 (
     input clka,
-    //input rsta, dout <= if(rst) '0 : .. ;
     input wea,//write enable
-    input ena,//bram  enable
     input [ADDR_WIDTH-1:0]addra, //address
     input [DATA_WIDTH-1:0]dina, //data_in
     output reg [DATA_WIDTH-1:0]douta //data_out
@@ -18,16 +17,10 @@ module BRAM#(
     reg [DATA_WIDTH-1:0] mem [RAM_DEPTH-1:0];
     //READ_FIRST Mode 
     always @(posedge clka)begin
-        if (ena)begin
           if(wea)begin
-            mem[addra] <= dina;
-           //douta<=dina;
+            mem[addra] <= #1 dina;
           end
-          douta <= mem[addra];
-          //else begin
-          //douta <= mem[addra];
-          //end
-        end
+          douta <= #1 mem[addra];
     end
     
 endmodule
