8d805ddd1316ed94b6f0f79cd2235f0ca249cd3a
berkarslanpencesi <berk.arslanpencesi41@gmail.com>
2025-12-20 18:24:41 +0300

    Rename write and read address signals in BRAM

--- fifov2/BRAM.sv
+++ fifov2/BRAM.sv
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
     //Read first BRAM
-    //WRITE_FIRST Yapmak için Bypass konulmali
+    //WRITE_FIRST Yapmak iÃ§in Bypass konulmali
 module BRAM#(
     parameter DATA_WIDTH=16,
     parameter ADDR_WIDTH=9
@@ -8,10 +8,9 @@ module BRAM#(
 (
     input clk,
     input wr_ea,//write enable
-    input [ADDR_WIDTH-1:0]wr_addr,
+    input [ADDR_WIDTH-1:0]addr,
     input [DATA_WIDTH-1:0]data_in, 
     
-    input [ADDR_WIDTH-1:0]rd_addr, //address  
     output reg [DATA_WIDTH-1:0]douta //data_out
     );
     localparam RAM_DEPTH= 1<<ADDR_WIDTH;
@@ -20,9 +19,9 @@ module BRAM#(
     //READ_FIRST Mode 
     always @(posedge clk)begin
           if(wr_ea)begin
-            mem[wr_addr] <= #1 data_in;
+            mem[addr] <= #1 data_in;
           end
-            douta <= #1 mem[rd_addr];
+            douta <= #1 mem[addr];
     end
    
 endmodule
